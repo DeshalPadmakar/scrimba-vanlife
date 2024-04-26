c84738bc5c0a7fe7b14f03de28f86ae1f5f241d5
deshal padmakar <cdeshalpadmakar@gmail.com>
2024-04-26 12:24:43 +0530

    removed strict mode

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,14 +5,12 @@ import Footer from "./Footer"
 
 export default function Layout() {
     return (
-        <React.StrictMode>
-            <div className="flex flex-col bg-white min-h-screen selection:bg-blue-300">
-                <Header />
-                <main>
-                    <Outlet />
-                </main>
-                <Footer />
-            </div>
-        </React.StrictMode>
+        <div className="flex flex-col bg-white min-h-screen selection:bg-blue-300">
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
     )
 }
